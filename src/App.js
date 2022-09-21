0e3cd1a4c4b175b038347a3f4b32583d2d0a10fc
Andrew Grenon <andrew.grenon@gmail.com>
2022-09-21 12:22:36 +1200

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+// ALL ABOUT USEEFFECT
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function updateCount() {
+    setCount(count + 1);
+  }
+
+  const [decrem, setDecrem] = useState(100);
+
+  function updateDecrem() {
+    setDecrem(decrem - 1);
+  }
+
+  function resetAll() {
+    setDecrem(100);
+    setCount(0);
+  }
+
+  useEffect(function () {
+    console.log("Something changed so we will log it");
+  });
+
+  useEffect(function () {
+    console.log("Only triggered once when component mounts");
+  }, []);
+
+  useEffect(
+    function () {
+      console.log(
+        "This will display only when the count state variable changes"
+      );
+    },
+    [count]
+  );
+
+  useEffect(
+    function () {
+      console.log(
+        "This will display only when the decrem state variable changes"
+      );
+    },
+    [decrem]
+  );
+
+  // USE CASE FOR THIS IS API
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="counter-flex">
+          <div className="counter-bord">
+            <div className="counter">Count: {count}</div>
+            <button onClick={updateCount} className="count">
+              Add one!
+            </button>
+          </div>
+          <div className="counter-bord">
+            <div className="counter">Count: {decrem}</div>
+            <button onClick={updateDecrem} className="decrem">
+              Minus one!
+            </button>
+          </div>
+        </div>
+        <button onClick={resetAll} className="reset">
+          Reset State
+        </button>
+      </div>
     </div>
   );
 }
